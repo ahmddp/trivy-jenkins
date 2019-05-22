be2ce52cefb423c49c4e4214b96f333be43df30f
ahmddp <ahmddp@gmail.com>
2019-05-22 12:23:25 +0300

    change scan command

--- cicd/scanDockerImage.sh
+++ cicd/scanDockerImage.sh
@@ -5,7 +5,7 @@ IMAGE=$1
 echo "The image to be scanned is: "$IMAGE
 
 # scan the image
-trivy --severity HIGH,CRITICAL --ignore-unfixed --exit-code 1 $IMAGE
+trivy --exit-code 1 --severity CRITICAL --quiet --auto-refresh $IMAGE
 
 # Get the exit code
 EXITCODE=$?
